7a13577dce6cca304c3951284cb2ac5024b4535a
lulavalenca <lulavalencafilho@gmail.com>
2025-09-08 17:54:52 -0300

    adicionando avg em avialição de movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,20 +1,10 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 from genres.models import Genre
 from actors.models import Actor
 
 
-#class MovieSerializer(serializers.Serializer):
-#    title = serializers.CharField()
-#    genre = serializers.PrimaryKeyRelatedField(
-#        queryset=Genre.objects.all(),
-#    )
-#    release_date = serializers.DateField()
-#    actors = serializers.PrimaryKeyRelatedField(
-#        queryset=Actor.objects.all(),
-#        many=True,
-#    )
-#    resume = serializers.CharField()
 
 class MovieModelSerializer(serializers.ModelSerializer):
     rate = serializers.SerializerMethodField(read_only=True)
@@ -24,19 +14,13 @@ class MovieModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
     def get_rate(self, obj):
-        reviews = obj.reviews.all()
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
         
-        if reviews:
-            sum_reviews = 0
-            
-            for review in reviews:
-                sum_reviews += review.stars
-               
-            reviews_count = reviews.count()
-            
-            return round(sum_reviews / reviews_count, 1)
+        if rate:
+            return rate
         
         return None
+
         
     def validate_release_date(self, value):
         if value.year < 1900:
