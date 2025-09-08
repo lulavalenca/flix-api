7b0a9a6729ce27ff45435e0adbb538788b4ef81f
lulavalenca <lulavalencafilho@gmail.com>
2025-09-08 16:19:52 -0300

    adicionando campos calculados no serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -4,20 +4,36 @@ from genres.models import Genre
 from actors.models import Actor
 
 
-class MovieSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    genre = serializers.PrimaryKeyRelatedField(
-        queryset=Genre.objects.all(),
-    )
-    release_date = serializers.DateField()
-    actors = serializers.PrimaryKeyRelatedField(
-        queryset=Actor.objects.all(),
-        many=True,
-    )
-    resume = serializers.CharField()
+#class MovieSerializer(serializers.Serializer):
+#    title = serializers.CharField()
+#    genre = serializers.PrimaryKeyRelatedField(
+#        queryset=Genre.objects.all(),
+#    )
+#    release_date = serializers.DateField()
+#    actors = serializers.PrimaryKeyRelatedField(
+#        queryset=Actor.objects.all(),
+#        many=True,
+#    )
+#    resume = serializers.CharField()
 
 class MovieModelSerializer(serializers.ModelSerializer):
-
+    rate = serializers.SerializerMethodField(read_only=True)
+    
     class Meta:
         model = Movie
         fields = '__all__'
+        
+    def get_rate(self, obj):
+        return 5
+        
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError("Resumo não deve ser maior do que 200 caracteres.")
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) < 200:
+            raise serializers.ValidationError("O resumo deve ter pelo menos 20 caracteres.")
+        return value  
+    
+    
