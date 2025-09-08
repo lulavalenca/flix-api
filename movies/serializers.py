4272c782c2e4dad359ba9e2cc4c880dd8d7e5118
lulavalenca <lulavalencafilho@gmail.com>
2025-09-08 17:38:49 -0300

    subindo serializers provisorio

--- movies/serializers.py
+++ movies/serializers.py
@@ -24,15 +24,27 @@ class MovieModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
     def get_rate(self, obj):
-        return 5
+        reviews = obj.reviews.all()
+        
+        if reviews:
+            sum_reviews = 0
+            
+            for review in reviews:
+                sum_reviews += review.stars
+               
+            reviews_count = reviews.count()
+            
+            return round(sum_reviews / reviews_count, 1)
+        
+        return None
         
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError("Resumo não deve ser maior do que 200 caracteres.")
         return value
     
     def validate_resume(self, value):
-        if len(value) < 200:
+        if len(value) < 500:
             raise serializers.ValidationError("O resumo deve ter pelo menos 20 caracteres.")
         return value  
     
